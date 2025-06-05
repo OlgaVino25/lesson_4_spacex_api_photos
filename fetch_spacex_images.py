1185788e56b0cf4ac51806d2d512e42830f446bc
OlgaVino25 <olgavinokyrova25@yandex.ru>
2025-06-05 14:08:47 +0300

    Update fetch_spacex_images.py

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -3,7 +3,7 @@ import requests
 from download_utils import download_images
 
 
-def fetch_spacex_photos(launch_id=None, download_folder='images', filename_number='spacex'):
+def fetch_spacex_photos(launch_id=None, folder='images', filename_number='spacex'):
     """Получает фото запуска SpaceX"""
     api_url = f'https://api.spacexdata.com/v5/launches/{launch_id or "latest"}'
 
@@ -14,7 +14,7 @@ def fetch_spacex_photos(launch_id=None, download_folder='images', filename_numbe
         photos = response.json().get('links', {}).get('flickr', {}).get('original', [])
         if photos:
             print(f'Найдено {len(photos)} фото. Скачиваю...')
-            download_images(photos, download_folder, filename_number)
+            download_images(photos, folder, filename_number)
             print('Готово!')
         else:
             print(f'Фото не найдены для запуска {launch_id or "latest"}')
@@ -26,11 +26,11 @@ def fetch_spacex_photos(launch_id=None, download_folder='images', filename_numbe
 def main():
     parser = argparse.ArgumentParser(description='Скачивание фото запусков SpaceX')
     parser.add_argument('--id', help='ID запуска (например: 5eb87d42ffd86e000604b384)\n''Оставьте пустым для последнего запуска')
-    parser.add_argument('--download_folder', default='spacex_images', help='Папка для сохранения (по умолчанию: spacex_images)')
-    parser.add_argument('--filename_number', default='spacex', help='Имя файлов (по умолчанию: spacex)')
+    parser.add_argument('--folder', default='spacex_images', help='Папка для сохранения')
+    parser.add_argument('--filename_number', default='spacex', help='Имя файлов')
     args = parser.parse_args()
     
-    fetch_spacex_photos(args.id, args.download_folder, args.filename_number)
+    fetch_spacex_photos(args.id, args.folder, args.filename_number)
 
 if __name__ == '__main__':
     main()
