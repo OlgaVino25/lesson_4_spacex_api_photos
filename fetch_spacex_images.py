812f0becba168ff19c717a2f765b31f63b9c506d
OlgaVino25 <olgavinokyrova25@yandex.ru>
2025-06-05 13:24:54 +0300

    Update fetch_spacex_images.py

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -1,32 +1,36 @@
+import argparse
 import requests
 from download_utils import download_images
 
 
-def fetch_spacex_last_launch(download_folder='images', filename_number='spacex'):
+def fetch_spacex_photos(launch_id=None, download_folder='images', filename_number='spacex'):
     """Получает фото запуска SpaceX"""
-    api_url = 'https://api.spacexdata.com/v5/launches/5eb87d42ffd86e000604b384'
+    api_url = f'https://api.spacexdata.com/v5/launches/{launch_id or "latest"}'
 
     try:
         response = requests.get(api_url)
         response.raise_for_status()
-        launch_photos = response.json()
-        photo_urls = launch_photos.get('links', {}).get('flickr', {}).get('original', [])
-        if not photo_urls:
-                print('Фотографии не найдены в запуске')
-                return
-        download_images(
-                image_urls=photo_urls,
-                folder=download_folder,
-                filename_number=filename_number
-            )
+
+        photos = response.json().get('links', {}).get('flickr', {}).get('original', [])
+        if photos:
+            print(f'Найдено {len(photos)} фото. Скачиваю...')
+            download_images(photos, download_folder, filename_number)
+            print('Готово!')
+        else:
+            print(f'Фото не найдены для запуска {launch_id or "latest"}')
     except requests.exceptions.RequestException as e:
         print(f'Ошибка при запросе к API SpaceX: {e}')
     except Exception as e:
         print(f'Непредвиденная ошибка: {e}')
 
-if __name__ == '__main__':
+def main():
+    parser = argparse.ArgumentParser(description='Скачивание фото запусков SpaceX')
+    parser.add_argument('--id', help='ID запуска (например: 5eb87d42ffd86e000604b384)\n''Оставьте пустым для последнего запуска')
+    parser.add_argument('--download_folder', default='spacex_images', help='Папка для сохранения (по умолчанию: spacex_images)')
+    parser.add_argument('--filename_number', default='spacex', help='Имя файлов (по умолчанию: spacex)')
+    args = parser.parse_args()
     
-    fetch_spacex_last_launch(
-        download_folder='spacex_images',
-        filename_number='spacex_launch'
-    )
+    fetch_spacex_photos(args.id, args.download_folder, args.filename_number)
+
+if __name__ == '__main__':
+    main()
