b6e32776abaf8fb4c3b1b51c1ff2704cd281e8da
OlgaVino25 <olgavinokyrova25@yandex.ru>
2025-06-12 09:28:14 +0300

    Update download_utils.py

--- download_utils.py
+++ download_utils.py
@@ -3,13 +3,13 @@ from pathlib import Path
 from file_utils import get_file_extension
 
 
-def download_images(image_urls, folder, filename_number):
+def download_images(image_urls, folder, filename_prefix):
     """Скачивает изображение по URL и сохраняет в указанную папку
     
     Args:
         image_urls: Ссылка на изображение
         folder: Папка для сохранения (объект Path или строка)
-        filename_number: Имя файла
+        filename_prefix: Имя файла
     """
     folder = Path(folder)
     folder.mkdir(parents=True, exist_ok=True)
@@ -20,7 +20,7 @@ def download_images(image_urls, folder, filename_number):
             response.raise_for_status()
 
             ext = get_file_extension(url) or '.jpg'
-            filename = f'{filename_number}_{index}{ext}'
+            filename = f'{filename_prefix}_{index}{ext}'
             filepath = folder / filename
         
             with open(filepath, 'wb') as file:
