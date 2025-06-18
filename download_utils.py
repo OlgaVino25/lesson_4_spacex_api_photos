60b22653034c61af194fca1151795b17bf487eb3
OlgaVino25 <olgavinokyrova25@yandex.ru>
2025-06-18 13:14:32 +0300

    Update download_utils.py

--- download_utils.py
+++ download_utils.py
@@ -5,7 +5,7 @@ from file_utils import get_file_extension
 
 def download_images(image_urls, folder, filename_prefix):
     """Скачивает изображение по URL и сохраняет в указанную папку
-    
+
     Args:
         image_urls: Ссылка на изображение
         folder: Папка для сохранения (объект Path или строка)
@@ -20,32 +20,6 @@ def download_images(image_urls, folder, filename_prefix):
         ext = get_file_extension(url) or '.jpg'
         filename = f'{filename_prefix}_{index}{ext}'
         filepath = folder / filename
-    
+
         with open(filepath, 'wb') as file:
             file.write(response.content)
-
-
-def handle_download_errors(image_urls, folder, filename_prefix):
-        """Обработка исключений"""
-        try:
-            download_images(image_urls, folder, filename_prefix)
-        except requests.exceptions.RequestException as e:
-            print(f'Ошибка сети: {e}')
-        except (IOError, OSError) as e:
-            print(f'Ошибка записи файла: {e}')
-        except KeyError as e:
-            print(f'Некорректный URL {url}: {e}')
-        except Exception as e:
-            print(f'Неизвестная ошибка при обработке {url}: {e}')
-            raise
-
-if __name__ == '__main__':
-    try:
-        handle_download_errors(
-            image_urls=['https://example.com/image1.jpg'],
-            folder='downloads',
-            filename_prefix='image'
-        )
-    except Exception as e:
-        print(f'Программа завершена с ошибкой: {e}')
-        exit(1)
